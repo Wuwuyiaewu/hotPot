9b2dd895244734d7a402473a4c413370dbd24712
健哲 <okman9527@gmail.com>
2019-11-17 01:51:01 +0800

    分類

--- project1/src/components/HelloWorld.vue
+++ project1/src/components/HelloWorld.vue
@@ -1,59 +1,90 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="row flex flex-just-center border">
+      <!-- 左側 -->
+      <div class="col-6 border">
+        <div class="row">
+          <div class="check">
+            <span>金額</span>
+          </div>
+          <div class="orderlist">
+            <span>點餐</span>
+          </div>
+          <div class="where">
+            <span>外帶</span>
+            <span>內用</span>
+          </div>
+        </div>
+      </div>
+      <!-- 右側 -->
+      <div class="col-6 border">
+        <div class="row">
+          <div class="type">
+            <table>
+              <tr>
+                <td class="col-4" v-for="item in type" :key="item">
+                  <button @click="addCatergory(item)">{{item}}</button>
+                  </td>
+              </tr>
+            </table>
+          </div>
+          <div class="catergory">
+            <button>{{catergoryName.name}}spicy</button>
+            <button>{{catergoryName.name}}japan</button>
+          </div>
+          <div class="another">
+            <button>nospicy</button>
+            <button>bigspicy</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      type: {
+        cow: 'cow',
+        sheep: 'sheep',
+        fish: 'fish',
+        pig: 'pig'
+      },
+      catergoryName: {
+        name: ''
+      }
+    }
+  },
+  methods: {
+    addCatergory (item) {
+      const vm = this
+      vm.catergoryName.name = item
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.flex{
+  display: flex;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.flex-just-center{
+  justify-content: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.row{
+  width: 100%;
 }
-a {
-  color: #42b983;
+.col-4{
+  width: 33.33333%;
+}
+.col-6{
+  width: 50%;
+}
+.border{
+  border: 1px solid #000;
 }
 </style>
