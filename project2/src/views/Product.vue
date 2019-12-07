17c52b3801a1c90f881c40240a012d3207cc9183
健哲 <okman9527@gmail.com>
2019-12-08 01:22:20 +0800

    item與false

--- project2/src/views/Product.vue
+++ project2/src/views/Product.vue
@@ -48,48 +48,47 @@
                         <div class="form-group">
                         <label for="image">輸入圖片網址</label>
                         <input type="text" class="form-control" id="image"
-                            placeholder="請輸入圖片連結">
+                            placeholder="請輸入圖片連結" v-model="tempProduct.imageUrl">
                         </div>
                         <div class="form-group">
                         <label for="customFile">或 上傳圖片
                             <i class="fas fa-spinner fa-spin"></i>
                         </label>
                         <input type="file" id="customFile" class="form-control"
-                            ref="files">
+                            ref="files" >
                         </div>
                         <img img="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=828346ed697837ce808cae68d3ddc3cf&auto=format&fit=crop&w=1350&q=80"
-                        class="img-fluid" alt="">
+                        class="img-fluid" alt="" :src="tempProduct.imageUrl">
                     </div>
                     <div class="col-sm-8">
                         <div class="form-group">
                         <label for="title">標題</label>
                         <input type="text" class="form-control" id="title"
-                            placeholder="請輸入標題">
+                            placeholder="請輸入標題" v-model="tempProduct.title">
                         </div>
 
                         <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="category">分類</label>
                             <input type="text" class="form-control" id="category"
-                            placeholder="請輸入分類">
+                            placeholder="請輸入分類" v-model="tempProduct.category">
                         </div>
                         <div class="form-group col-md-6">
                             <label for="price">單位</label>
                             <input type="unit" class="form-control" id="unit"
-                            placeholder="請輸入單位">
+                            placeholder="請輸入單位" v-model="tempProduct.unit">
                         </div>
                         </div>
-
                         <div class="form-row">
                         <div class="form-group col-md-6">
                         <label for="origin_price">原價</label>
                             <input type="number" class="form-control" id="origin_price"
-                            placeholder="請輸入原價">
+                            placeholder="請輸入原價" v-model="tempProduct.origin_price">
                         </div>
                         <div class="form-group col-md-6">
                             <label for="price">售價</label>
                             <input type="number" class="form-control" id="price"
-                            placeholder="請輸入售價">
+                            placeholder="請輸入售價" v-model="tempProduct.price">
                         </div>
                         </div>
                         <hr>
@@ -97,18 +96,18 @@
                         <div class="form-group">
                         <label for="description">產品描述</label>
                         <textarea type="text" class="form-control" id="description"
-                            placeholder="請輸入產品描述"></textarea>
+                            placeholder="請輸入產品描述" v-model="tempProduct.description"></textarea>
                         </div>
                         <div class="form-group">
                         <label for="content">說明內容</label>
                         <textarea type="text" class="form-control" id="content"
-                            placeholder="請輸入產品說明內容"></textarea>
+                            placeholder="請輸入產品說明內容" v-model="tempProduct.content"></textarea>
                         </div>
                         <div class="form-group">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox"
-                            id="is_enabled">
-                            <label class="form-check-label" for="is_enabled">
+                            id="is_enabled" v-model="tempProduct.is_enabled">
+                            <label class="form-check-label" for="is_enabled" :true-value="1" :false-value="0">
                             是否啟用
                             </label>
                         </div>
@@ -118,7 +117,7 @@
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">取消</button>
-                    <button type="button" class="btn btn-primary">確認</button>
+                    <button type="button" class="btn btn-primary" @click="updateProduct">確認</button>
                 </div>
                 </div>
             </div>
@@ -134,6 +133,8 @@ export default {
     data(){
         return{
             products:[],
+            tempProduct:{},
+            isNew:false
         }
     },
     methods:{
@@ -157,8 +158,28 @@ export default {
                 vm.products = res.data.products
             })
         },
-        openModal(){
+        openModal(isNew,item){
             $('#productModal').modal('show')
+            if(isNew){
+                this.isNew = true
+                this.tempProduct = {}
+            }else{
+                this.isNew = false
+                this.tempProduct = Object.assign({},item)
+            }
+        },
+        updateProduct(){
+            const url = `${process.env.VUE_APP_APIPATH}/api/${process.env.VUE_APP_USERPATH}/admin/product`
+            const vm = this
+            vm.axios.post(url,{data:vm.tempProduct}).then(res=>{
+                if(res.data.success){
+                    $('#productModal').modal('hide')
+                    vm.getProducts()
+                }else{
+                    vm.getProducts()
+                    console.log('訊息失敗')
+                }
+            })
         }
     },
     created(){
