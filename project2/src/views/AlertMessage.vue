b829862f5b1b6dbbcae5517eb5dba85a01840007
jian <okman9527@gmail.com>
2019-12-17 17:19:14 +0800

    emit

--- project2/src/views/AlertMessage.vue
+++ project2/src/views/AlertMessage.vue
@@ -20,6 +20,8 @@ export default {
   name: 'Navbar',
   data() {
     return {
+      // 定義 message 為陣列
+      // 方便後續推入 (push) 內容進去
       messages: [{
         message: '內容',
         status: 'danger',
@@ -29,12 +31,15 @@ export default {
   },
   methods: {
     updateMessage(message, status) {
+      // 定義 id 數值
+      // 時間就是僅此唯一的 id
       const timestamp = Math.floor(new Date() / 1000);
       this.messages.push({
         message,
         status,
         timestamp,
       });
+      // 訊息跳出後，過一段時間必須移除
       this.removeMessageWithTiming(timestamp);
     },
     // 透過 click 事件傳入key
@@ -45,9 +50,13 @@ export default {
     // 設置時間設定
     removeMessageWithTiming(timestamp) {
       const vm = this;
+      // 設定延遲時間
       setTimeout(() => {
+        // 找出每一個 item  訊息
         vm.messages.forEach((item, i) => {
+          // 如果該訊息 id 等於 updateMessage 方法內所執行傳入的 id 
           if (item.timestamp === timestamp) {
+            // 對該 item 的 key 數值位置 做刪除一個的動作
             vm.messages.splice(i, 1);
           }
         });
@@ -60,6 +69,7 @@ export default {
     // 自定義名稱 'messsage:push'
     // message: 傳入參數
     // status: 樣式，預設值為 warning
+    // 使用$on 去註冊名稱，給予 $emit 去使用觸發
     vm.$bus.$on('message:push', (message, status = 'warning') => {
       vm.updateMessage(message, status);
     });
